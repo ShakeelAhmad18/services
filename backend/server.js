4e7704f6a3d12939a7db79c5a2957c7e2cf83faf
Shakeel Ahmad <150515418+ShakeelAhmad18@users.noreply.github.com>
2024-12-19 10:05:05 +0500

    service provider project

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const bodyParser=require('body-parser')
+const cookieParser=require('cookie-parser')
+const cors=require('cors')
+
+
+const app=express();
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin:[],
+    credentials:true
+}))
+
+
+const PORT=5000;
+
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`The server is running on PORT ${PORT}`)
+    })
+})
+
+app.get('/',(req,res)=>{
+   res.send('My backend')
+})
+
+
+
+
+/*app.listen(PORT,()=>{
+    console.log(`The server is running on PORT ${PORT}`)
+})*/
